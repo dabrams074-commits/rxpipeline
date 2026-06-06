39526d20006db5ab3a914036d317e2aec76f6c03
dabrams074-commits <dabrams074@gmail.com>
2026-06-06 15:30:58 -0400

    TESTING: set trial to 0 days to test paywall

--- netlify/functions/check-subscription.js
+++ netlify/functions/check-subscription.js
@@ -29,7 +29,7 @@ exports.handler = async (event) => {
   // 10-day free trial based on account creation date
   if (createdAt) {
     const daysSinceSignup = (Date.now() - new Date(createdAt).getTime()) / (1000 * 60 * 60 * 24);
-    if (daysSinceSignup < 10) {
+    if (daysSinceSignup < 0) { // TESTING: set back to 10 for production
       return {
         statusCode: 200,
         headers: { ...CORS, 'Content-Type': 'application/json' },
